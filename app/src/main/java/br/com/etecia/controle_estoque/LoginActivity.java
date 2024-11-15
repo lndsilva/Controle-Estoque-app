18ac871cd741ca1f0c0e8ac832d19420f6d1e4c1
lndsilva <lndsilva@hotmail.com>
2024-11-14 22:11:43 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/controle_estoque/LoginActivity.java
@@ -0,0 +1,42 @@
+package br.com.etecia.controle_estoque;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView txtCadastre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_layout);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        txtCadastre = findViewById(R.id.txtCadastre);
+
+        txtCadastre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),CriarUsuarioActivity.class));
+                finish();
+            }
+        });
+
+
+    }
+}
